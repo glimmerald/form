af8fbc0f070d9e186be35922827ff9d1d7f6335a
glimmerald <glimmerald@gmail.com>
2020-06-04 17:08:51 +0300

    try to  add js

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
-var form = document.querySelector('.form-with-validation')
-var validateBtn = form.querySelector( '.validate-btn')
+const form = document.querySelector('.form-with-validation');
+const name = document.getElementById('name');
+
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
-    console.log('clicked on validate')
+    if (name.value === '') {
+    document.getElementById('name-message').classList.add("show")
+    }
 })
